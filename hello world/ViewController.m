1845b3baec378dcde0f0368a9c4732f5492e8218
牛筋草 <1073280651@qq.com>
2015-12-04 12:57:33 +0800

    no message

---
+++ hello world/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  hello world
+//
+//  Created by 牛筋草 on 15/12/4.
+//  Copyright © 2015年 牛筋草. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ hello world/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  hello world
+//
+//  Created by 牛筋草 on 15/12/4.
+//  Copyright © 2015年 牛筋草. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
